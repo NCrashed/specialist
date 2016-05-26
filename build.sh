63c27094be8e3742b142459873ef2014867f2ac3
NCrashed <ncrashed@gmail.com>
2016-05-26 13:40:49 +0300

    Abstract of the paper

--- build.sh
+++ build.sh
@@ -1,26 +1,28 @@
 #!/usr/bin/sh
+cp $1/src/rpz/referat.tex $1/build/referat.tex 
+
 mkdir -p $1/build/orgecon && cd $1/build/orgecon
 cp $1/src/Common.hs $1/build/orgecon
 cp $1/src/pgf-pie.sty $1/build
-$1/compile.sh $1/src/rpz/orgecon/stages
-$1/compile.sh $1/src/rpz/orgecon/costs
-$1/compile.sh $1/src/rpz/orgecon/workers
-$1/compile.sh $1/src/rpz/orgecon/salary
-$1/compile.sh $1/src/rpz/orgecon/equip
-$1/compile.sh $1/src/rpz/orgecon/workplace
-$1/compile.sh $1/src/rpz/orgecon/overheads
-$1/compile.sh $1/src/rpz/orgecon/others
+$1/$2 $1/src/rpz/orgecon/stages
+$1/$2 $1/src/rpz/orgecon/costs
+$1/$2 $1/src/rpz/orgecon/workers
+$1/$2 $1/src/rpz/orgecon/salary
+$1/$2 $1/src/rpz/orgecon/equip
+$1/$2 $1/src/rpz/orgecon/workplace
+$1/$2 $1/src/rpz/orgecon/overheads
+$1/$2 $1/src/rpz/orgecon/others
 
 mkdir -p $1/build/obzeco && cd $1/build/obzeco
 cp $1/build/orgecon/*.hs $1/build/obzeco
 cp $1/src/rpz/obzeco/*.png $1/build/obzeco
-$1/compile.sh $1/src/rpz/obzeco/analysis
-$1/compile.sh $1/src/rpz/obzeco/normalize
-$1/compile.sh $1/src/rpz/obzeco/expertise
-$1/compile.sh $1/src/rpz/obzeco/results
+$1/$2 $1/src/rpz/obzeco/analysis
+$1/$2 $1/src/rpz/obzeco/normalize
+$1/$2 $1/src/rpz/obzeco/expertise
+$1/$2 $1/src/rpz/obzeco/results
 
 cd $1/build
-$1/compile.sh $1/src/rpz
+$1/$2 $1/src/rpz
 
 cp $1/src/utf8gost705u.bst $1/build
 cp $1/src/biblio.bib $1/build
---
+++ compile_memo.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/sh
+stack exec -- haskintex -overwrite -verbose -keep -memo $1.htex
